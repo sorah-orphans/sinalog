7c19f909de92d5d873029d7544c8232834e3c7b9
sora_harakami <sora134@gmail.com>
2009-07-23 08:21:37 +0900

    sinalog.rb +ActiveRecord::Base.establish +class Log

--- sinalog.rb
+++ sinalog.rb
@@ -1,3 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'active_record'
+###############################
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :dbfile => 'data/data.sqlite'
+)
+
+class Log < ActiveRecord::Base
+end
+
