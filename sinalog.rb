fc9f98f314eb9f1074eb3d542e754eebb93ca2f7
sora_harakami <sora134@gmail.com>
2009-07-23 11:09:33 +0900

    Add edit features

--- sinalog.rb
+++ sinalog.rb
@@ -19,3 +19,40 @@ end
 get '/' do
     haml :index, :locals => {:logs => Log.find(:all)}
 end
+get '/log/:id' do |n|
+    begin
+        log = Log.find(n)
+    rescue
+        "Not Found"
+    else
+      haml :log, :locals => {:log => Log.find(n)}
+    end
+end
+
+get '/edit/?' do
+    haml :edit, :locals => {:entryid => nil}
+end
+get '/edit/:id' do |n|
+    begin
+        log = Log.find(n)
+    rescue
+        "Not Found"
+    else
+        haml :edit, :locals => {:entryid => n,:entry => log}
+    end
+end
+
+post '/edit' do
+    if !params[:id]
+        log = Log.create(:title => params[:title],:body => params[:body].gsub("\n","<br/>"))
+        redirect "/log/#{log.id}"
+    else
+        begin
+            Log.update(params[:id],:title => params[:title],:body => params[:body].gsub("\n","<br/>"))
+            redirect "/log/#{params[:id]}"
+        rescue
+            "Not Found"
+        end
+    end
+end
+    
